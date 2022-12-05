5b853e455631b83669eb46a5a33bdececec629f1
neddinn <>
2022-12-05 16:49:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import { ChatGPTAPI } from 'chatgpt';
+import { SocketModeClient } from '@slack/socket-mode';
+import { WebClient } from '@slack/web-api';
+
+const appToken = process.env.SLACK_APP_TOKEN;
+const socketModeClient = new SocketModeClient({ appToken });
+const webClient = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+(async () => {
+  // Connect to Slack
+  await socketModeClient.start();
+})();
+
+async function sendMessage(text) {
+  const api = new ChatGPTAPI({ sessionToken: process.env.SESSION_TOKEN });
+  console.log('ensuring auth...')
+  await api.ensureAuth();
+  console.log('sending message...')
+  const response = await api.sendMessage(text);
+  console.log('Response', response)
+  return response;
+}
+
+const isValid = (event) => {
+  const { text, channel_type } = event;
+  if (channel_type !== 'im') return false;
+  if (!text || text.trim().length === 0) return false;
+  // To avoid an infinite loop when the bot reponds
+  if (event.bot_profile?.id) return false;
+  return true;
+}
+
+socketModeClient.on('message', async ({ event, _, ack }) => {
+  try {
+    await ack();
+    const { text } = event;
+    if (!isValid(event)) return;
+    console.log('prompt..', text);
+
+    const response = await sendMessage(text);
+
+    await webClient.chat.postMessage({
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: response,
+          },
+        },
+      ],
+      channel: event.channel,
+    });
+  } catch (error) {
+    console.log('An error occurred', error);
+  }
+});
